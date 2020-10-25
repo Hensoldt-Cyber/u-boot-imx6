boundarydevices/u-boot

fuse: make fuse read set an environment variable
Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

cmd/fuse.c
@@ -63,14 +63,19 @@ static int do_fuse(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 
 		printf("Reading bank %u:\n", bank);
 		for (i = 0; i < cnt; i++, word++) {
+			char name[16];
+			char vbuf[16];
 			if (!(i % 4))
-				printf("\nWord 0x%.8x:", word);
+				printf("\nWord 0x%.8x: ", word);
 
 			ret = fuse_read(bank, word, &val);
 			if (ret)
 				goto err;
 
-			printf(" %.8x", val);
+			snprintf(name, sizeof(name), "fuse_read_val%x", i);
+			snprintf(vbuf, sizeof(vbuf), "%.8x", val);
+			setenv(name, vbuf);
+			puts(vbuf);
 		}
 		putc('\n');
 	} else if (!strcmp(op, "sense")) {

Hensoldt-Cyber/u-boot-imx6

cmd/fuse.c
@@ -63,14 +63,19 @@ static int do_fuse(struct cmd_tbl *cmdtp, int flag, int argc,
 
 		printf("Reading bank %u:\n", bank);
 		for (i = 0; i < cnt; i++, word++) {
+			char name[16];
+			char vbuf[16];
 			if (!(i % 4))
-				printf("\nWord 0x%.8x:", word);
+				printf("\nWord 0x%.8x: ", word);
 
 			ret = fuse_read(bank, word, &val);
 			if (ret)
 				goto err;
 
-			printf(" %.8x", val);
+			snprintf(name, sizeof(name), "fuse_read_val%x", i);
+			snprintf(vbuf, sizeof(vbuf), "%.8x", val);
+			env_set(name, vbuf);
+			puts(vbuf);
 		}
 		putc('\n');
 	} else if (!strcmp(op, "sense")) {