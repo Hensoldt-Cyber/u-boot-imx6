boundarydevices/u-boot

arch/arm/imx-common/cpu: add CONFIG_RESET_CAUSE_ADDR

arch/arm/imx-common/cpu.c
@@ -28,6 +28,14 @@ char *get_reset_cause(void)
 	cause = readl(&src_regs->srsr);
 	writel(cause, &src_regs->srsr);
 
+#ifdef CONFIG_RESET_CAUSE_ADDR
+	{
+		unsigned *p = (unsigned *)CONFIG_RESET_CAUSE_ADDR;
+		*p++ = 0x12345678;	/* magic value to check for */
+		*p = cause;
+	}
+#endif
+
 	switch (cause) {
 	case 0x00001:
 	case 0x00011:

Hensoldt-Cyber/u-boot-imx6

arch/arm/mach-imx/cpu.c
@@ -47,6 +47,13 @@ u32 get_imx_reset_cause(void)
 /* preserve the value for U-Boot proper */
 #if !defined(CONFIG_SPL_BUILD)
 		writel(reset_cause, &src_regs->srsr);
+#if defined(CONFIG_RESET_CAUSE_ADDR)
+		{
+			unsigned *p = (unsigned *)CONFIG_RESET_CAUSE_ADDR;
+			*p++ = 0x12345678;	/* magic value to check for */
+			*p = reset_cause;
+		}
+#endif
 #endif
 	}
 