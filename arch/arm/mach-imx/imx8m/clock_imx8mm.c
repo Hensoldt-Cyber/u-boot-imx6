51561658b7c0a73f920a1c6e4ec8f89e0b66eb75
Gary Bisson <gary.bisson@boundarydevices.com>
2020-11-11 17:38:09 +0100

    fix MLK-18243-6 arm: imx8m: add clock driver for i.MX8MM
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- arch/arm/mach-imx/imx8m/clock_imx8mm.c
+++ arch/arm/mach-imx/imx8m/clock_imx8mm.c
@@ -20,7 +20,7 @@ DECLARE_GLOBAL_DATA_PTR;
 
 static struct anamix_pll *ana_pll = (struct anamix_pll *)ANATOP_BASE_ADDR;
 
-#ifdef CONFIG_SECURE_BOOT
+#ifdef CONFIG_IMX_HAB
 void hab_caam_clock_enable(unsigned char enable)
 {
 	/* The CAAM clock is always on for iMX8M */
