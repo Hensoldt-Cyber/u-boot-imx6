Hensoldt-Cyber/u-boot-imx6

commit 76bcf789fa54c9d3381001de8908b2be3aed85e9

MLK-17044-2 HAB: Add SIP call for ARM64 platform
When current EL is not EL3, the direct calling to HAB will fail because
CAAM/SNVS can't initialize at non-secure mode. In this case, we use
SIP call to run the HAB in ATF.

Signed-off-by: Ye Li <ye.li@nxp.com>
(cherry picked from commit bca4e251b6083476e987d92c428136a1c81bcb0b)
(cherry picked from commit e31a75cc78ad2d5e6ab5d58851058ab57de1566d)

arch/arm/mach-imx/hab.c
@@ -51,6 +51,14 @@ static int verify_ivt_header(struct ivt_header *ivt_hdr)
 }
 
 #ifdef CONFIG_ARM64
+#define FSL_SIP_HAB		0xC2000007
+#define FSL_SIP_HAB_AUTHENTICATE	0x00
+#define FSL_SIP_HAB_ENTRY		0x01
+#define FSL_SIP_HAB_EXIT		0x02
+#define FSL_SIP_HAB_REPORT_EVENT	0x03
+#define FSL_SIP_HAB_REPORT_STATUS	0x04
+#define FSL_SIP_HAB_FAILSAFE		0x05
+#define FSL_SIP_HAB_CHECK_TARGET	0x06
 static volatile gd_t *gd_save;
 #endif
 
@@ -79,6 +87,15 @@ enum hab_status hab_rvt_report_event(enum hab_status status, uint32_t index,
 	hab_rvt_report_event_t *hab_rvt_report_event_func;
 	hab_rvt_report_event_func =  (hab_rvt_report_event_t *)HAB_RVT_REPORT_EVENT;
 
+#if defined(CONFIG_ARM64)
+	if (current_el() != 3) {
+		/* call sip */
+		ret = (enum hab_status)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_REPORT_EVENT, (unsigned long)index,
+			(unsigned long)event, (unsigned long)bytes);
+		return ret;
+	}
+#endif
+
 	save_gd();
 	ret = hab_rvt_report_event_func(status, index, event, bytes);
 	restore_gd();
@@ -94,6 +111,15 @@ enum hab_status hab_rvt_report_status(enum hab_config *config,
 	hab_rvt_report_status_t *hab_rvt_report_status_func;
 	hab_rvt_report_status_func = (hab_rvt_report_status_t *)HAB_RVT_REPORT_STATUS;
 
+#if defined(CONFIG_ARM64)
+	if (current_el() != 3) {
+		/* call sip */
+		ret = (enum hab_status)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_REPORT_STATUS,
+			(unsigned long)config, (unsigned long)state, 0);
+		return ret;
+	}
+#endif
+
 	save_gd();
 	ret = hab_rvt_report_status_func(config, state);
 	restore_gd();
@@ -106,6 +132,14 @@ enum hab_status hab_rvt_entry(void)
 	enum hab_status ret;
 	hab_rvt_entry_t *hab_rvt_entry_func;
 	hab_rvt_entry_func = (hab_rvt_entry_t *)HAB_RVT_ENTRY;
+
+#if defined(CONFIG_ARM64)
+	if (current_el() != 3) {
+		/* call sip */
+		ret = (enum hab_status)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_ENTRY, 0, 0, 0);
+		return ret;
+	}
+#endif
 
 	save_gd();
 	ret = hab_rvt_entry_func();
@@ -120,6 +154,14 @@ enum hab_status hab_rvt_exit(void)
 	hab_rvt_exit_t *hab_rvt_exit_func;
 	hab_rvt_exit_func =  (hab_rvt_exit_t *)HAB_RVT_EXIT;
 
+#if defined(CONFIG_ARM64)
+	if (current_el() != 3) {
+		/* call sip */
+		ret = (enum hab_status)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_EXIT, 0, 0, 0);
+		return ret;
+	}
+#endif
+
 	save_gd();
 	ret = hab_rvt_exit_func();
 	restore_gd();
@@ -132,6 +174,14 @@ void hab_rvt_failsafe(void)
 	hab_rvt_failsafe_t *hab_rvt_failsafe_func;
 	hab_rvt_failsafe_func = (hab_rvt_failsafe_t *)HAB_RVT_FAILSAFE;
 
+#if defined(CONFIG_ARM64)
+	if (current_el() != 3) {
+		/* call sip */
+		call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_FAILSAFE, 0, 0, 0);
+		return;
+	}
+#endif
+
 	save_gd();
 	hab_rvt_failsafe_func();
 	restore_gd();
@@ -143,6 +193,15 @@ enum hab_status hab_rvt_check_target(enum hab_target type, const void *start,
 	enum hab_status ret;
 	hab_rvt_check_target_t *hab_rvt_check_target_func;
 	hab_rvt_check_target_func =  (hab_rvt_check_target_t *)HAB_RVT_CHECK_TARGET;
+
+#if defined(CONFIG_ARM64)
+	if (current_el() != 3) {
+		/* call sip */
+		ret = (enum hab_status)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_CHECK_TARGET, (unsigned long)type,
+			(unsigned long)start, (unsigned long)bytes);
+		return ret;
+	}
+#endif
 
 	save_gd();
 	ret = hab_rvt_check_target_func(type, start, bytes);
@@ -157,6 +216,15 @@ void *hab_rvt_authenticate_image(uint8_t cid, ptrdiff_t ivt_offset,
 	void *ret;
 	hab_rvt_authenticate_image_t *hab_rvt_authenticate_image_func;
 	hab_rvt_authenticate_image_func = (hab_rvt_authenticate_image_t *)HAB_RVT_AUTHENTICATE_IMAGE;
+
+#if defined(CONFIG_ARM64)
+	if (current_el() != 3) {
+		/* call sip */
+		ret = (void *)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_AUTHENTICATE, (unsigned long)ivt_offset,
+			(unsigned long)start, (unsigned long)bytes);
+		return ret;
+	}
+#endif
 
 	save_gd();
 	ret = hab_rvt_authenticate_image_func(cid, ivt_offset, start, bytes, loader);

commit b13e9b68ecbaaa655692c86af383fdb99c7ed847

imx: hab: fix SIP calls to be standard
Fixes: 76bcf789fa ("MLK-17044-2 HAB: Add SIP call for ARM64 platform")

Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

arch/arm/mach-imx/hab.c
@@ -14,6 +14,7 @@
 #include <asm/arch/clock.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/mach-imx/hab.h>
+#include <linux/arm-smccc.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -94,10 +95,10 @@ enum hab_status hab_rvt_report_event(enum hab_status status, uint32_t index,
 
 #if defined(CONFIG_ARM64)
 	if (current_el() != 3) {
-		/* call sip */
-		ret = (enum hab_status)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_REPORT_EVENT, (unsigned long)index,
-			(unsigned long)event, (unsigned long)bytes);
-		return ret;
+		struct arm_smccc_res res;
+		arm_smccc_smc(FSL_SIP_HAB, FSL_SIP_HAB_REPORT_EVENT,
+			      index, event, bytes, 0, 0, 0, &res);
+		return res.a0;
 	}
 #endif
 
@@ -118,10 +119,10 @@ enum hab_status hab_rvt_report_status(enum hab_config *config,
 
 #if defined(CONFIG_ARM64)
 	if (current_el() != 3) {
-		/* call sip */
-		ret = (enum hab_status)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_REPORT_STATUS,
-			(unsigned long)config, (unsigned long)state, 0);
-		return ret;
+		struct arm_smccc_res res;
+		arm_smccc_smc(FSL_SIP_HAB, FSL_SIP_HAB_REPORT_STATUS,
+			      config, state, 0, 0, 0, 0, &res);
+		return res.a0;
 	}
 #endif
 
@@ -140,9 +141,10 @@ enum hab_status hab_rvt_entry(void)
 
 #if defined(CONFIG_ARM64)
 	if (current_el() != 3) {
-		/* call sip */
-		ret = (enum hab_status)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_ENTRY, 0, 0, 0);
-		return ret;
+		struct arm_smccc_res res;
+		arm_smccc_smc(FSL_SIP_HAB, FSL_SIP_HAB_ENTRY,
+			      0, 0, 0, 0, 0, 0, &res);
+		return res.a0;
 	}
 #endif
 
@@ -161,9 +163,10 @@ enum hab_status hab_rvt_exit(void)
 
 #if defined(CONFIG_ARM64)
 	if (current_el() != 3) {
-		/* call sip */
-		ret = (enum hab_status)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_EXIT, 0, 0, 0);
-		return ret;
+		struct arm_smccc_res res;
+		arm_smccc_smc(FSL_SIP_HAB, FSL_SIP_HAB_EXIT,
+			      0, 0, 0, 0, 0, 0, &res);
+		return res.a0;
 	}
 #endif
 
@@ -181,9 +184,10 @@ void hab_rvt_failsafe(void)
 
 #if defined(CONFIG_ARM64)
 	if (current_el() != 3) {
-		/* call sip */
-		call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_FAILSAFE, 0, 0, 0);
-		return;
+		struct arm_smccc_res res;
+		arm_smccc_smc(FSL_SIP_HAB, FSL_SIP_HAB_FAILSAFE,
+			      0, 0, 0, 0, 0, 0, &res);
+		return res.a0;
 	}
 #endif
 
@@ -201,10 +205,10 @@ enum hab_status hab_rvt_check_target(enum hab_target type, const void *start,
 
 #if defined(CONFIG_ARM64)
 	if (current_el() != 3) {
-		/* call sip */
-		ret = (enum hab_status)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_CHECK_TARGET, (unsigned long)type,
-			(unsigned long)start, (unsigned long)bytes);
-		return ret;
+		struct arm_smccc_res res;
+		arm_smccc_smc(FSL_SIP_HAB, FSL_SIP_HAB_CHECK_TARGET,
+			      type, start, bytes, 0, 0, 0, &res);
+		return res.a0;
 	}
 #endif
 
@@ -224,10 +228,10 @@ void *hab_rvt_authenticate_image(uint8_t cid, ptrdiff_t ivt_offset,
 
 #if defined(CONFIG_ARM64)
 	if (current_el() != 3) {
-		/* call sip */
-		ret = (void *)call_imx_sip(FSL_SIP_HAB, FSL_SIP_HAB_AUTHENTICATE, (unsigned long)ivt_offset,
-			(unsigned long)start, (unsigned long)bytes);
-		return ret;
+		struct arm_smccc_res res;
+		arm_smccc_smc(FSL_SIP_HAB, FSL_SIP_HAB_AUTHENTICATE,
+			      ivt_offset, start, bytes, 0, 0, 0, &res);
+		return res.a0;
 	}
 #endif
 